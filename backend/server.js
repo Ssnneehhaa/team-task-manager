411e3c4ccccd5ad544116615c6d6eb5e21412f31
Sneha Singh <snehahaha2004@gmail.com>
2026-05-17 09:57:14 +0530

    Fixed production CORS

--- backend/server.js
+++ backend/server.js
@@ -6,47 +6,14 @@ require("dotenv").config();
 
 const app = express();
 
-// Railway / Production Port
-const PORT = process.env.PORT || 5000;
-
-// Middleware
 app.use(
     cors({
-        origin: "*"
+        origin: [
+            "http://localhost:5173",
+            "https://team-task-manager-bay-sigma.vercel.app"
+        ],
+        credentials: true
     })
 );
 
 app.use(express.json());
-
-// Test Route
-app.get("/", (req, res) => {
-    res.send("API Running");
-});
-
-// Routes
-app.use("/api/auth", require("./routes/authRoutes"));
-
-app.use("/api/test", require("./routes/testRoutes"));
-
-app.use("/api/projects", require("./routes/projectRoutes"));
-
-app.use("/api/tasks", require("./routes/taskRoutes"));
-
-// Database Connection
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-
-    console.log("MongoDB Connected");
-
-    app.listen(PORT, () => {
-
-        console.log(`Server running on port ${PORT}`);
-
-    });
-
-})
-.catch((err) => {
-
-    console.log("MongoDB Error:", err.message);
-
-});
